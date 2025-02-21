0f124d1b3e65610c47aa27659946646f65dbecaa
dhynarazarine <dinar.andiyani@gmail.com>
2025-02-21 19:45:51 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,41 @@ Route::get('/', function () {
 
 Route::resource('items', ItemController::class);
 // Route::resource metode yang secara otomatis berdasarkan metode yang ada di ItemController
+
+// Route Hello
+Route::get('/hello', function () {
+        return 'Hello World';
+});
+// Route World
+Route::get('/world', function () {
+        return 'World';
+});
+// Route '/'
+Route::get('/', function () {
+        return 'Selamat Datang';
+});
+// Route about
+Route::get('/about', function () {
+        return 'NIM : 2341760109, Nama : Queenadhynar Azarine Dwipa Andiyani';
+});
+
+
+// Route username
+Route::get('/user/{name}', function ($name) {
+        return 'Nama saya '.$name;
+  });
+
+// Route dengan 1 parameter lebih
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+        return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+// Route Halaman Artikel
+Route::get('/articles/{id}', function ($id) {
+        return 'Halaman Artikel dengan ID '.$id;
+  });
+
+// Route User
+Route::get('/user/{name?}', function ($name="John") {
+        return 'Nama saya  '.$name;
+  });
